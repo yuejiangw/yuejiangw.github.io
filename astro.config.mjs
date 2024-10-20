5b22100d6a640aa76bc3d8dd271d0d0bdda0010c
Yuejiang Wu <yuejiangw@indeed.com>
2024-10-19 17:14:16 -0700

    config github.io url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://yuejiangw.github.io',
 	integrations: [
 		starlight({
 			title: 'Knowledge Base',
